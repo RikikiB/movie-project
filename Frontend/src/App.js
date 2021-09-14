e7b741fcc4716d1e62f20af2676e4668073b9437
Rekishia Brooks <rikkib27007@yahoo.com>
2021-09-14 11:40:55 -0700

    updates

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css'
+import useState from 'react'
+
+const [body, setBody] = useState()
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
+     <div className='App'>
+      <button className='btn1'>GENRES</button>
+      <button className='btn2'>YEAR</button>
+      <button className='btn3'>RATING</button>
+      <button className='btn4'>ACTOR</button>
+      <input type='text' placeholder='Search..' name='search'></input>
+    <div className="container">
+    <h2>Don't know what to watch?</h2>
+    <button className="btn5">
+      <h1>START QUIZ!</h1></button>
+      </div>
   )
 }
 
