581d5d1f9f494be1db7490eeedc7b9c57500478a
Rekishia Brooks <rikkib27007@yahoo.com>
2021-09-15 10:31:53 -0700

    header, footer app js changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,13 @@
 import './App.css'
-
-
+import Footer from '.Components/Footer'
+import Header from '.Components/Header'
 
 function App() {
   return (
-     <div className='App'>
-      <button className='btn1'>GENRES</button>
-      <button className='btn2'>YEAR</button>
-      <button className='btn3'>RATING</button>
-      <button className='btn4'>ACTOR</button>
-      <input type='text' placeholder='Search..' name='search'></input>
-      </div>
-    <div className="container">
-    <h2>Don't know what to watch?</h2>
-    <button className="btn5">
-      <h1>START QUIZ!</h1></button>
-      </div>
+    <div className='App'>
+      <Footer />
+      <Header />
+    </div>
   )
 }
 
