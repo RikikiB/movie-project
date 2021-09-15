d1425e313dc434a65b745b8b61e2509178952c01
Sam Gax <samgaxiola702@gmail.com>
2021-09-15 13:25:57 -0700

    fixing css

--- Frontend/src/Components/Header.tsx
+++ Frontend/src/Components/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './mysass.scss'
+import './Header.scss'
 
 function Header() {
   return (
     <div className='Header'>
-      <button className='btn1'>GENRES</button>
-      <button className='btn2'>YEAR</button>
-      <button className='btn3'>RATING</button>
-      <button className='btn4'>ACTOR</button>
+      <button className='btn'>GENRES</button>
+      <button className='btn'>YEAR</button>
+      <button className='btn'>RATING</button>
+      <button className='btn'>ACTOR</button>
       <input type='text' placeholder='Search..' name='search'></input>
       <div className='container'>
         <h2>Don't know what to watch?</h2>
