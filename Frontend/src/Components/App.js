8f1dd34d7fa7c2f87720365d1999ff4a38de1d0a
Rekishia Brooks <rikkib27007@yahoo.com>
2021-09-16 11:06:32 -0700

    edited layout

--- Frontend/src/Components/App.js
+++ Frontend/src/Components/App.js
@@ -1,20 +1,30 @@
-import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import MovieDetailPage from "./views/MovieDetailPage";
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+import Home from './Home'
+import MovieDetailPage from './views/MovieDetailPage'
+import { Link } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home />
-            <div>home</div>
+            <div>
+              <Link to='/quizpage'></Link>
+              <button className='btn5'>
+                <h1>START QUIZ!</h1>
+              </button>
+            </div>
           </Route>
-          <Route path="/detail">
+          <Route path='/detail'>
             <MovieDetailPage />
           </Route>
         </Switch>
@@ -22,7 +32,7 @@ function App() {
         <Footer />
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- Frontend/src/Components/Home.tsx
+++ Frontend/src/Components/Home.tsx
@@ -1,18 +1,12 @@
-import { Link } from "react-router-dom";
-import "./Home.css";
+import './Home.css'
 function Home() {
   return (
     <div>
-      <div className="container">
+      <div className='container'>
         <h2>Don't know what to watch?</h2>
-        <Link to="/quizpage">
-          <button className="btn5">
-            <h1>START QUIZ!</h1>
-          </button>
-        </Link>
       </div>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
