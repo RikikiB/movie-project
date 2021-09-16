a74612c46c314d02ad20ada7b311d655695197bd
Sam Gax <samgaxiola702@gmail.com>
2021-09-16 11:08:03 -0700

    doing code for quiz page

--- Frontend/src/Components/App.js
+++ Frontend/src/Components/App.js
@@ -1,28 +1,31 @@
-import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import MovieDetailPage from "./views/MovieDetailPage";
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './Home'
+import MovieDetailPage from './views/MovieDetailPage'
+import Quizpage from './Quizpage'
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home />
-            <div>home</div>
           </Route>
-          <Route path="/detail">
+          <Route path='/detail'>
             <MovieDetailPage />
+          </Route>
+          <Route path='/quiz'>
+            <Quizpage />
           </Route>
         </Switch>
 
         <Footer />
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
