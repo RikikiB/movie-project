37f7eef24be74d0f9312963d20b1c3646c5b09be
Rekishia Brooks <rikkib27007@yahoo.com>
2021-09-16 13:08:21 -0700

    quizpage imported

--- Frontend/src/Components/App.js
+++ Frontend/src/Components/App.js
@@ -1,12 +1,7 @@
 import './App.css'
 import Footer from './Footer'
 import Header from './Header'
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './Home'
-import MovieDetailPage from './views/MovieDetailPage'
 import Quizpage from './Quizpage'
-=======
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,7 +10,6 @@ import {
 import Home from './Home'
 import MovieDetailPage from './views/MovieDetailPage'
 import { Link } from 'react-router-dom'
->>>>>>> 8f1dd34d7fa7c2f87720365d1999ff4a38de1d0a
 function App() {
   return (
     <div className='App'>
@@ -24,15 +18,12 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <Home />
-<<<<<<< HEAD
-=======
             <div>
               <Link to='/quizpage'></Link>
               <button className='btn5'>
                 <h1>START QUIZ!</h1>
               </button>
             </div>
->>>>>>> 8f1dd34d7fa7c2f87720365d1999ff4a38de1d0a
           </Route>
           <Route path='/detail'>
             <MovieDetailPage />
