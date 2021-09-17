d68e4971a4f1c52368a8d1933a9997627bdb53f7
Sheida100 <sh.nilizadeh@yahoo.com>
2021-09-17 09:42:55 -0700

    api for fetching picture of actors

--- Frontend/src/Components/App.js
+++ Frontend/src/Components/App.js
@@ -1,50 +1,36 @@
-import './App.css'
-import Footer from './Footer'
-import Header from './Header'
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './Home'
-import MovieDetailPage from './views/MovieDetailPage'
-import Quizpage from './Quizpage'
-=======
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom'
-import Home from './Home'
-import MovieDetailPage from './views/MovieDetailPage'
-import { Link } from 'react-router-dom'
->>>>>>> 8f1dd34d7fa7c2f87720365d1999ff4a38de1d0a
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import MovieDetailPage from "./views/MovieDetailPage";
+import Quizpage from "./Quizpage";
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Header />
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Home />
-<<<<<<< HEAD
-=======
             <div>
-              <Link to='/quizpage'></Link>
-              <button className='btn5'>
+              <Link to="/quizpage"></Link>
+              <button className="btn5">
                 <h1>START QUIZ!</h1>
               </button>
             </div>
->>>>>>> 8f1dd34d7fa7c2f87720365d1999ff4a38de1d0a
           </Route>
-          <Route path='/detail'>
+          <Route path="/detail">
             <MovieDetailPage />
           </Route>
-          <Route path='/quiz'>
+          <Route path="/quiz">
             <Quizpage />
           </Route>
         </Switch>
         <Footer />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/Components/views/MovieDetailPage.js
+++ Frontend/src/Components/views/MovieDetailPage.js
@@ -1,63 +1,82 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { Descriptions, Badge } from "antd";
+import  './MovieDetailPage.scss'
 
 function MovieDetailPage(props) {
-  const omdb_api_key = "65b23a68";
-  const movieId = "tt3896198";
+  // const omdb_api_key = "65b23a68";
   // const URL_api = `http://www.omdbapi.com/?i=${movieId}&apikey=${omdb_api_key}`;
-  const URL_api =
-    "https://api.themoviedb.org/3/movie/550?api_key=795cf19c05b9ff607f4b7206a0a4abd3";
+  const omdb_api_key = "795cf19c05b9ff607f4b7206a0a4abd3";
+  const movieId = "tt3896198";
 
-  const [movie, setMovie] = useState([]);
+  const URL_api = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${omdb_api_key}`;
+
+  const [movie, setMovie] = useState({genres:[]});
   const [show, setShow] = useState(false);
-  const [crew, setCrew] = useState([]);
+  const [crews, setCrews] = useState([]);
 
   useEffect(() => {
     fetch(URL_api)
       .then((response) => response.json())
       .then((response) => {
         setMovie(response);
-        console.log(response);
+        });
 
-        fetch(
-          `http://www.omdbapi.com/?i=${movieId}/credits?&apikey=${omdb_api_key}`
-        )
-          .then((response) => response.json())
-          .then((respose) => setCrew(response));
-      });
+    fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${omdb_api_key}`)
+      .then((res) => res.json())
+      .then((res) => setCrews(res.cast))
+
   }, []);
+
+console.log(crews)
+
   return (
     <div>
       <Descriptions title="Movie Info" bordered>
-        <Descriptions.Item label="Title : ">{movie.Title}</Descriptions.Item>
-        <Descriptions.Item label="Director : ">
-          {movie.Director}
+        <Descriptions.Item label="Title : ">
+          {movie.original_title}
         </Descriptions.Item>
-        <Descriptions.Item label="imdbRating : ">
-          {movie.imdbRating}
+        <Descriptions.Item label="release_date : ">
+          {movie.release_date}
         </Descriptions.Item>
-        <Descriptions.Item label="Genre : ">{movie.Genre}</Descriptions.Item>
-        <Descriptions.Item label="Country : ">
-          {movie.Country}
+
+        <Descriptions.Item label="vote_count : ">
+          {movie.vote_count}
         </Descriptions.Item>
-        <Descriptions.Item label="imdbVotes : ">
-          {movie.imdbVotes}
+        <Descriptions.Item label="runtime : ">
+          {movie.runtime}
         </Descriptions.Item>
-        <Descriptions.Item label="Writer : ">{movie.Writer}</Descriptions.Item>
-        <Descriptions.Item label="Runtime : ">
-          {movie.Runtime}
+        <Descriptions.Item label="vote_average : ">
+          {movie.vote_average}
         </Descriptions.Item>
-        <Descriptions.Item label="Released : ">
-          {movie.Released}
+        <Descriptions.Item label="popularity : ">
+          {movie.popularity}
+        </Descriptions.Item>
+        <Descriptions.Item label="revenue : ">
+          {movie.revenue}
+        </Descriptions.Item>
+        <Descriptions.Item label="status: ">{movie.status}</Descriptions.Item>
+
+        <Descriptions.Item label="genres : ">
+          {movie.genres.map(function (typ) {
+              return typ.name;
+            }).join(" ")}
         </Descriptions.Item>
       </Descriptions>
       <button className="actor-view" onClick={() => setShow(!show)}>
         Actor view
       </button>
       <div className={show ? "" : "hidden"}>
-        <h1>{movie.Actors}</h1>
+        <h1>{movie.poster_path}</h1>
       </div>
+      {/* Grid cars for crews */}
+      {/* <div className="show_pic_actor">
+        {crews && crews.map(function(crew)
+         {return image={crew.poster_path}})}
+      </div> */}
+      {crews.map((item) => {
+        return <img src={`https://www.themoviedb.org/t/p/w276_and_h350_face${item.profile_path}`}></img>
+      })}
     </div>
   );
 }
