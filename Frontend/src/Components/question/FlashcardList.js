0ea435ecf4650d7cbe75278910bcbcb36e6a353f
Sam Gax <samgaxiola702@gmail.com>
2021-09-29 09:30:02 -0700

    added question ID to flashcardlist

--- Frontend/src/Components/question/FlashcardList.js
+++ Frontend/src/Components/question/FlashcardList.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-import "./Flashcard.scss";
-import Flashmain from "./Flashmain.js";
+import { useState, useEffect } from 'react'
+import './Flashcard.scss'
 
 function Answer({ answer, handleChange, selectedAnswer }) {
-  const inputId = `${answer.answer}_${answer.id}`;
-  console.log({ selectedAnswer });
+  const inputId = `${answer.answer}_${answer.id}`
   return (
-    <div className="answer">
+    <div className='answer'>
       <label htmlFor={inputId}>
         <input
           id={inputId}
-          className="answerinput"
-          type="radio"
+          className='answerinput'
+          type='radio'
           checked={answer.id == selectedAnswer}
           value={answer.id}
           onChange={(e) => handleChange(e.target.value)}
@@ -19,29 +17,68 @@ function Answer({ answer, handleChange, selectedAnswer }) {
         {answer.answer}
       </label>
     </div>
-  );
+  )
 }
 
 function FlashcardList({ currentQuestion }) {
-  const [selectedAnswer, setSelectedAnswer] = useState();
-  if (!currentQuestion) return null;
+  const [userAnswers, setUserAnswers] = useState([])
+  const [selectedAnswer, setSelectedAnswer] = useState('')
+  useEffect(() => {
+    const previousAnswer = userAnswers?.find(
+      (a) => a.questionId === currentQuestion.questionId
+    )
+    setSelectedAnswer(previousAnswer ? previousAnswer : selectedAnswer)
+  }, [currentQuestion, userAnswers, selectedAnswer])
+  if (!currentQuestion) return null
 
   return (
-    <div className="questionairecontainer">
-      <div className="questioncontainer">
+    <div className='questionairecontainer'>
+      <div className='questioncontainer'>
         <div>{currentQuestion.question}</div>
       </div>
-      <div className="answercontainer">
+      <div className='answercontainer'>
+        {/* TODO: KEEP SELECTED ANSWER ACROSS RENDERS */}
         {currentQuestion?.answers?.map((answer) => (
           <Answer
             key={answer.id}
             answer={answer}
-            handleChange={(answerId) => setSelectedAnswer(answerId)}
+            handleChange={(answerId) => {
+              console.log(currentQuestion, answerId)
+              // find the questionId
+              const answerExists = Boolean(
+                userAnswers.find(
+                  (ans) => ans.questionId === currentQuestion.questionId
+                )
+              )
+              if (answerExists) {
+                setUserAnswers((prev) =>
+                  prev.map((userAnswer) => {
+                    if (userAnswer.questionId === currentQuestion.questionId) {
+                      return { ...userAnswer, answerId }
+                    } else {
+                      return userAnswer
+                    }
+                  })
+                )
+              } else {
+                setUserAnswers([
+                  ...userAnswers,
+                  {
+                    questionId: currentQuestion.questionId,
+                    answerId,
+                  },
+                ])
+              }
+              setSelectedAnswer(answerId)
+            }}
             selectedAnswer={selectedAnswer}
           />
         ))}
       </div>
+      <div style={{ color: 'white' }}>
+        {JSON.stringify(userAnswers, null, 2)}
+      </div>
     </div>
-  );
+  )
 }
-export default FlashcardList;
+export default FlashcardList
--- Frontend/src/Components/question/Flashmain.js
+++ Frontend/src/Components/question/Flashmain.js
@@ -1,67 +1,68 @@
-import { useState, useEffect } from "react";
-import FlashcardList from "./FlashcardList";
-import "./Flashcard.scss";
+import { useState, useEffect } from 'react'
+import FlashcardList from './FlashcardList'
+import './Flashcard.scss'
 
 function Flashmain() {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([])
   // current question index below
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
   useEffect(() => {
     async function getAnswerByQuestion() {
       try {
         const result = await fetch(
-          "http://localhost:8000/getAnswerByQuestion",
+          'http://localhost:8000/getAnswerByQuestion',
           {
-            method: "GET",
-            header: { "content-type": "application/json" },
+            method: 'GET',
+            header: { 'content-type': 'application/json' },
           }
-        );
-        const data = await result.json();
-        console.log({ data });
+        )
+        const data = await result.json()
         const questionsAndAnswers = data.reduce((retval, el) => {
           if (el.question in retval) {
             retval[el.question].answers.push({
+              questionId: el.questionid,
               id: el.answerid,
               answer: el.answer,
-            });
+            })
           } else {
             retval[el.question] = {
+              questionId: el.questionid,
               question: el.question,
               answers: [{ id: el.answerid, answer: el.answer }],
-            };
+            }
           }
-          return retval;
-        }, {});
-        setQuestions(Object.values(questionsAndAnswers));
+          return retval
+        }, {})
+        setQuestions(Object.values(questionsAndAnswers))
       } catch (error) {
-        console.log("Error:", error);
+        console.log('Error:', error)
       }
     }
-    getAnswerByQuestion();
-  }, []);
+    getAnswerByQuestion()
+  }, [])
 
   function nextQuestion() {
     if (currentQuestionIndex <= questions.length - 1) {
-      setCurrentQuestionIndex((prev) => prev + 1);
+      setCurrentQuestionIndex((prev) => prev + 1)
     }
   }
   function prevQuestion() {
     if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex((prev) => prev - 1);
+      setCurrentQuestionIndex((prev) => prev - 1)
     }
   }
   return (
-    <div className="flash-main">
+    <div className='flash-main'>
       <FlashcardList currentQuestion={questions[currentQuestionIndex]} />
-      <div className="show-page">
-        <button className="previousbutton" onClick={prevQuestion}>
+      <div className='show-page'>
+        <button className='previousbutton' onClick={prevQuestion}>
           Previous Question!
         </button>
-        <button className="nextbutton" onClick={nextQuestion}>
+        <button className='nextbutton' onClick={nextQuestion}>
           Next Question!
         </button>
       </div>
     </div>
-  );
+  )
 }
-export default Flashmain;
+export default Flashmain
